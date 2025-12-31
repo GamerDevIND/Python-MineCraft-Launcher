41891e230e9f99a65b33c09bd47eaf348c838c88
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2025-12-31 18:10:36 +0530

    Add files via upload

--- launch.py
+++ launch.py
@@ -4,43 +4,80 @@ from downloader import version_data, json_path
 import uuid 
 import subprocess 
 import os 
+
 def build_class_path():
     base_lib_dir = os.path.join(DOWNLOAD_DIR, 'client', 'JAR', 'libraries')
     libs = []
+
     for root, _, files in os.walk(base_lib_dir):
         for file in files:
             if file.endswith('.jar'):
                 libs.append(os.path.join(root, file))
 
-    client_path = os.path.join(DOWNLOAD_DIR,'client', 'JAR', 'client.jar')
-
+    client_path = os.path.join(DOWNLOAD_DIR, 'client', 'JAR', 'client.jar')
+    
     classpath = os.pathsep.join(libs + [client_path])
     return classpath
 
-RAM_ARGS = f'-Xmx{MAX_RAM_GB}G -Xms{MIN_RAM_GB}G'   
+def create_profile_json(username, version_id):
+    """Creates a standard launcher_profiles.json for compatibility."""
+    profile_data = {
+        "profiles": {
+            "CustomProfile": {
+                "name": f"Vanilla - {version_id}",
+                "gameDir": os.path.abspath(DOWNLOAD_DIR),
+                "lastVersionId": version_id,
+                "javaArgs": f"-Xmx{MAX_RAM_GB}G -Xms{MIN_RAM_GB}G",
+                "type": "custom"   
+            }
+        },
+        "selectedProfile": "CustomProfile",
+        "clientToken": str(uuid.uuid4()),
+        "authenticationDatabase": {}
+    }
+
+    profile_path = os.path.join(DOWNLOAD_DIR, 'launcher_profiles.json')
+    with open(profile_path, 'w') as f:
+        json.dump(profile_data, f, indent=4)
+    print(f"Profile JSON created at: {profile_path}")
+
 
 if version_data and json_path: 
-    with open(file=json_path) as f: 
+    with open(json_path, 'r') as f: 
         json_file = json.load(f) 
-        classpath = build_class_path() 
-        main_class = json_file['mainClass'] 
-        asset_index = json_file['assetIndex']['id']
-        NATIVES_DIR = os.path.join(DOWNLOAD_DIR, 'client', 'natives') 
-        main_class = json_file['mainClass'] 
-        uuid_offline = str(uuid.uuid4()) 
-        asset_index = json_file['assetIndex']['id'] 
-        version_id = DESIRED_VERSION
-        print('Success. Launching') 
-        launch_command = ( 
-            f'java {RAM_ARGS} '
-	    f'-Djava.library.path="{NATIVES_DIR}" ' 
-            f'-cp "{classpath}" ' f'{main_class} ' f'--version {version_id} ' 
-            f'--gameDir "{DOWNLOAD_DIR}" ' f'--assetsDir "{DOWNLOAD_DIR}/assets" ' 
-            f'--assetIndex {asset_index} ' f'--uuid {uuid_offline} ' f'--accessToken 0 ' 
-            f'--userType legacy ' f'--versionType release ' f'--username {USERNAME} ' 
-            f'--launchTarget {main_class}' 
-	) 
         
-        with open('launcher.log' ,'w') as f: 
-            print("log file opened")
-            subprocess.run(launch_command, stderr=subprocess.STDOUT, stdout=f, check=True)
+    classpath = build_class_path() 
+    main_class = json_file['mainClass'] 
+    asset_index = json_file['assetIndex']['id']
+    natives_dir = os.path.join(DOWNLOAD_DIR, 'client', 'natives') 
+    uuid_offline = str(uuid.uuid4()) 
+    ram_args = f'-Xmx{MAX_RAM_GB}G -Xms{MIN_RAM_GB}G'
+
+    create_profile_json(USERNAME, DESIRED_VERSION)
+
+    launch_command = ( 
+        f'java {ram_args} '
+        f'-Djava.library.path="{natives_dir}" ' 
+        f'-cp "{classpath}" ' 
+        f'{main_class} ' 
+        f'--version {DESIRED_VERSION} ' 
+        f'--gameDir "{DOWNLOAD_DIR}" ' 
+        f'--assetsDir "{DOWNLOAD_DIR}/assets" ' 
+        f'--assetIndex {asset_index} ' 
+        f'--uuid {uuid_offline} ' 
+        f'--accessToken 0 ' 
+        f'--userType legacy ' 
+        f'--versionType release ' 
+        f'--username {USERNAME} ' 
+        f'--launchTarget {main_class}' 
+    ) 
+    
+    with open('launcher.log', 'w') as log_file: 
+        print('log file opened')
+        print('Launching client...')
+        try:
+            subprocess.run(launch_command, stderr=subprocess.STDOUT, stdout=log_file, shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"❌ Game crashed or failed to start. Check launcher.log for details.")
+else:
+    print("❌ Error: Missing version data or JSON path. Run downloader first.")
