7b1d3af1549533f747ccdb3388cdd93cf5e06fa2
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2026-02-11 01:30:38 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ USERNAME = "GamerDevIND"
 DOWNLOAD_DIR = "minecraft_downloads"
 MAX_RAM_GB = 3
 MIN_RAM_GB = 3
-DESIRED_VERSION = '1.21.8'
+DESIRED_VERSION = '1.21.11'
 
 OS_TYPE = None
 arch_suffix  = ''
