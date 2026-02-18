e53c7f763dcbca7addd9e517424c1b031aad8c71
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2026-02-19 00:38:09 +0530

    Add files via upload

--- configs.py
+++ configs.py
@@ -6,7 +6,9 @@ USERNAME = "GamerDevIND"
 DOWNLOAD_DIR = "minecraft_downloads"
 MAX_RAM_GB = 3
 MIN_RAM_GB = 3
-DESIRED_VERSION = '1.21.11'
+DESIRED_VERSION = '1.21.10'
+
+FABRIC_PREFIX = "fabric-loader-"
 
 OS_TYPE = None
 arch_suffix  = ''
